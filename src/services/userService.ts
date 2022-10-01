07cfcb9e19b01573563b00adadcd9b5981334af1
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-10-01 17:27:48 -0300

    feat: add user service draft

---
+++ src/services/userService.ts
@@ -0,0 +1,9 @@
+import { ICreateNewUserData } from "../types/userTypes";
+
+export async function getUserByEmail(userEmail: string) {
+  return;
+}
+
+export async function signUpNewUser(newUserData: ICreateNewUserData) {
+  return;
+}
