86d8ea3a5acc58b15e14754fca810cbe203b9c2b
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-10-03 12:13:10 -0300

    feat: add signUpNewUser to user service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import { ICreateNewUserData } from "../types/userTypes";
 import * as userRepository from "../repositories/userRepository";
 
@@ -8,5 +9,19 @@ export async function getUserByEmail(userEmail: string) {
 }
 
 export async function signUpNewUser(newUserData: ICreateNewUserData) {
+  const user = await getUserByEmail(newUserData.email);
+  if (user) {
+    throw { type: "conflict", message: "This email is already registered" };
+  }
+  const SALT = 10;
+  const encryptedPassword = bcrypt.hashSync(newUserData.password, SALT);
+
+  const createNewUserData: ICreateNewUserData = {
+    email: newUserData.email,
+    password: encryptedPassword,
+  };
+
+  await userRepository.insertNewUser(createNewUserData);
+
   return;
 }
