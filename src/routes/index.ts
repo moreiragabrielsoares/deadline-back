7a9589a8439df3a4f128b33be396d53df14d234f
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-10-03 22:55:38 -0300

    feat: add process router to index router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import signUpRouter from "./signUpRouter";
 import loginRouter from "./loginRouter";
+<<<<<<< Updated upstream
+=======
+import processRouter from "./processRouter";
+>>>>>>> Stashed changes
 
 const router = Router();
 
 router.use(signUpRouter);
 router.use(loginRouter);
+<<<<<<< Updated upstream
+=======
+router.use(processRouter);
+>>>>>>> Stashed changes
 
 export default router;
