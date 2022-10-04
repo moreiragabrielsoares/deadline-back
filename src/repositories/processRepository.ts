46aeafcfeb1bfc6841764ff93d56d8e5db01b601
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-10-04 00:23:49 -0300

    fix: solve the problem of duplicated process task

--- src/repositories/processRepository.ts
+++ src/repositories/processRepository.ts
@@ -6,3 +6,19 @@ export async function insertNewProcess(newProcessData: ICreateNewProcess) {
     data: newProcessData,
   });
 }
+
+export async function getProcessByUserIdProcessNumberTask(
+  userId: number,
+  processNumber: string,
+  task: string | null
+) {
+  const process = await prisma.processes.findFirst({
+    where: {
+      userId,
+      processNumber,
+      task,
+    },
+  });
+
+  return process;
+}
--- src/services/processService.ts
+++ src/services/processService.ts
@@ -5,6 +5,16 @@ export async function createNewProcessDeadline(
   newProcessData: INewProcess,
   userId: number
 ) {
+  const process = await processRepository.getProcessByUserIdProcessNumberTask(
+    userId,
+    newProcessData.processNumber,
+    newProcessData.task
+  );
+
+  if (process) {
+    throw { type: "conflict", message: "Process task already registered" };
+  }
+
   const createNewProcessData = { ...newProcessData, userId };
   createNewProcessData.deadline = new Date(createNewProcessData.deadline);
 
